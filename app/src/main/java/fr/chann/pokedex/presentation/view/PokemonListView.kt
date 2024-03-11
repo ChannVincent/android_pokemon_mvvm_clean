79217212ccb31b4b7e947b9632e850408225c61e
Vincent Chann <>
2024-03-11 03:20:56 +0100

    P1 : setup project : presentation layer

--- app/src/main/java/fr/chann/pokedex/presentation/view/PokemonListView.kt
+++ app/src/main/java/fr/chann/pokedex/presentation/view/PokemonListView.kt
@@ -5,17 +5,36 @@ import androidx.navigation.NavController
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.Card
+import androidx.compose.runtime.collectAsState
 import androidx.lifecycle.viewmodel.compose.viewModel
 import fr.chann.pokedex.presentation.viewmodel.PokemonListViewModel
+import fr.chann.pokedex.presentation.viewstate.PokemonListViewState
 
 
 @Composable
 fun PokemonListView(navController: NavController) {
     val viewModel: PokemonListViewModel = viewModel()
+    val viewState = viewModel.viewState.collectAsState()
+    viewModel.loadPokemonList()
     Column {
         Text(text = "LIST VIEW")
         Button(onClick = { navController.navigate("pokemon_detail") }) {
             Text("Go to Pokemon Detail")
+        }
+        when (val state = viewState.value) {
+            is PokemonListViewState.Loading -> Text(text = "Loading")
+            is PokemonListViewState.Content -> {
+                state.pokemonList.forEach { pokemon ->
+                    Card {
+                        Text(text = pokemon.id)
+                        Text(text = pokemon.title)
+                        Text(text = pokemon.description)
+                        Text(text = pokemon.image)
+                    }
+                }
+            }
+            is PokemonListViewState.Error -> Text(text = "Error")
         }
     }
 }
--- app/src/main/java/fr/chann/pokedex/presentation/viewmodel/PokemonListViewModel.kt
+++ app/src/main/java/fr/chann/pokedex/presentation/viewmodel/PokemonListViewModel.kt
@@ -3,16 +3,25 @@ package fr.chann.pokedex.presentation.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import fr.chann.pokedex.presentation.viewstate.PokemonListViewState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class PokemonListViewModel: ViewModel() {
 
-    private val _viewState = MutableLiveData<PokemonListViewState>()
-    val viewState: LiveData<PokemonListViewState>
-        get() = _viewState
+    private val _viewState = MutableStateFlow<PokemonListViewState>(PokemonListViewState.Loading)
+    val viewState: StateFlow<PokemonListViewState> = _viewState
 
-    fun loadProductList() {
-        // todo
+    fun updateViewState(newState: PokemonListViewState) {
+        viewModelScope.launch {
+            _viewState.value = newState
+        }
+    }
+
+    fun loadPokemonList() {
+        // TODO
     }
 }
 
