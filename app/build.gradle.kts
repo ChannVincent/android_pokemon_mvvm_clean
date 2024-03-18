92dc3a7d9a74eaf214e13fc7b96bdb991334a759
Vincent Chann <>
2024-03-18 18:39:40 +0100

    P7 : detail view

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,11 +58,13 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
 
     // jetpack compose
+    val composeVersion = "1.6.3"
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    implementation("androidx.compose.ui:ui-graphics:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
+    implementation("androidx.compose.ui:ui-util:$composeVersion")
     implementation("androidx.compose.material3:material3")
     implementation("com.github.bumptech.glide:compose:1.0.0-beta01")
 
--- app/src/main/java/fr/chann/pokedex/presentation/view/PokemonDetailScreen.kt
+++ app/src/main/java/fr/chann/pokedex/presentation/view/PokemonDetailScreen.kt
@@ -1,13 +1,32 @@
 package fr.chann.pokedex.presentation.view
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.util.lerp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
@@ -15,8 +34,11 @@ import com.bumptech.glide.integration.compose.GlideImage
 import fr.chann.pokedex.presentation.event.PokemonDetailEvent
 import fr.chann.pokedex.presentation.viewmodel.PokemonDetailViewModel
 import fr.chann.pokedex.presentation.viewstate.PokemonDetailViewState
+import kotlin.math.absoluteValue
 
-@OptIn(ExperimentalGlideComposeApi::class, ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalGlideComposeApi::class,
+    ExperimentalFoundationApi::class
+)
 @Composable
 fun PokemonDetailScreen(navController: NavController, pokemonId: String, viewModel: PokemonDetailViewModel = hiltViewModel()) {
     LaunchedEffect(Unit) {
@@ -29,19 +51,38 @@ fun PokemonDetailScreen(navController: NavController, pokemonId: String, viewMod
         }) {
             Text("< Back")
         }
-        Text(text = "DETAIL VIEW $pokemonId")
         when (val state = viewState.value) {
             is PokemonDetailViewState.Loading -> Text(text = "Loading")
             is PokemonDetailViewState.Content -> {
                 val pokemon = state.pokemon
-                Card (onClick = {
-                    navController.navigate("pokemon_detail/${pokemon.id}")
-                }) {
-                    Text(text = pokemon.id)
-                    Text(text = pokemon.title)
-                    Text(text = pokemon.description)
-                    GlideImage(model = pokemon.image, contentDescription = null)
+                val pagerState = rememberPagerState(pageCount = {
+                    2
+                })
+                HorizontalPager(state = pagerState) { page ->
+                    Card(
+                        shape = RoundedCornerShape(corner = CornerSize(0.dp)),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .graphicsLayer {
+                                val pageOffset = (
+                                        (pagerState.currentPage - page) + pagerState
+                                            .currentPageOffsetFraction
+                                        ).absoluteValue
+                                alpha = lerp(
+                                    start = 0.5f,
+                                    stop = 1f,
+                                    fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                                )
+                            }
+                    ) {
+                        GlideImage(
+                            model = if (page == 0) pokemon.image else pokemon.imageShiny,
+                            contentDescription = null,
+                            modifier = Modifier.aspectRatio(ratio = 1f)
+                        )
+                    }
                 }
+                Text(text = "#${pokemon.id} ${pokemon.title}", fontSize = 25.sp)
             }
             is PokemonDetailViewState.Error -> Text(text = "Error")
         }
